42285691719b8a8ab693d9d088a1513d23c0ce57
Evan Moses <evanmoses@gmail.com>
2021-02-02 22:45:57 -0500

    link backend to react app

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const testBackendRouter = require('./routes/testbackend');
 
 const app = express();
 
@@ -13,6 +15,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/testbackend', testBackendRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
