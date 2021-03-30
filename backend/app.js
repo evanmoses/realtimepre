cc4f8fd146f76f2deab2f97ae19333fb1bab10ba
Evan Moses <evanmoses@gmail.com>
2021-03-29 21:40:36 -0400

    additional express-generator cleanup

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,12 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const testBackendRouter = require('./routes/testbackend');
 const rangesRouter = require('./routes/ranges');
 
 const app = express();
@@ -22,14 +18,11 @@ app.set('view engine', 'pug');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/ranges', rangesRouter);
 
 // catch 404 and forward to error handler
